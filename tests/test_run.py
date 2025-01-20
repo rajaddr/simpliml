1e3fe299df78047c1301c8e8e2de954b0370f33b
rajaddr <rajaddr@gmail.com>
2025-01-20 12:42:00 +0530

    Code Push

--- tests/test_run.py
+++ tests/test_run.py
@@ -2,6 +2,6 @@ import pytest, pandas as pd, simpliml.timeseriesforecast as tsf, warnings
 warnings.filterwarnings("ignore")
 
 def test_add():
-    sourceDF = pd.read_csv("tests\AirPassengers.csv")
+    sourceDF = pd.read_csv("tests/AirPassengers.csv")
     dataDF, futureDF = tsf.generateTSData(sourceDF, format='%Y-%m', freq='MS', periods=30)
     #mdlResult = tsf.runModel(dataDF, futureDF, seasonal=12, modelApproach='FAST', testSize=80)
